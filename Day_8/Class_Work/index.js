5add7732a663bb707c38d92d1c70b4d3162fdb26
Subrata <subratabera032@gmail.com>
2025-02-21 09:03:06 +0530

    Readme_file

---
+++ Day_8/Class_Work/index.js
@@ -0,0 +1,26 @@
+document.getElementById("parent").style.backgroundColor="green";
+document.getElementById("parent").style.border="1 px solid black";
+document.getElementById("parent").style.height="200px";
+document.getElementById("parent").style.width="300px";
+
+let btn=document.createElement("button");
+
+btn.innerText="submit";
+
+btn.style.backgroundColor="yellow";
+btn.style.padding="10px";
+btn.style.marginLeft="10px";
+document.getElementById("parent").append(btn);
+
+var num=10;
+console.log(num);
+num=15;
+console.log(num)
+
+let nu=10;
+console.log(nu);
+
+nu=15;
+function myFunc(){
+    con
+}
