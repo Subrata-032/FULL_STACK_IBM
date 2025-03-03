2241b5eb2249575833f3688340b9f2197cb132f6
Subrata <subratabera032@gmail.com>
2025-03-03 11:55:25 +0530

    all_files

---
+++ Day_14/Class_Work/script1.js
@@ -0,0 +1,61 @@
+function getData(){
+    let parseData = JSON.parse(localStorage.getItem("cartData"));
+    console.log(parseData);
+    let cart=document.getElementById("full_cart");
+    parseData.forEach((el,index)=>{
+        let div=document.createElement("div");
+        div.className="cartItem";
+
+        let button=document.createElement("button");
+        button.innerText="Purchase";
+
+        let heading=document.createElement("h3");
+        heading.innerText=el.category;
+
+        let img=document.createElement("img");
+        img.src=`${el.image}`;
+
+        let name=document.createElement("h3");
+        name.innerText=el.name;
+
+        let rating=document.createElement("h3");
+        rating.innerText=el.rating;
+
+        let price=document.createElement("h3");
+        price.innerText=el.price;
+
+        let select=document.createElement("select");
+        select.innerText="<option>1</option><option>2</option><option>3</option><option>4</option>"
+
+        let button1=document.createElement("button");
+        button1.innerText="Remove Item";
+
+        button1.addEventListener("click",function(){
+            alert("Item removed successfully");
+        })
+
+        button.addEventListener("click",function(){
+            alert("Item purchased successfully");
+        });
+        div.append(heading,img,select,name,rating,price,button,button1);
+        cart.append(div);
+    });
+}
+getData();
+let home=document.getElementById("home");
+home.addEventListener("click",function(){
+    window.location.href="index.htm";
+});
+function removeElement(el){
+    console.log(el);
+}
+let container=document.querySelector("#full_cart");
+
+function removeFromCart(itemId){
+    let parseData=JSON.parse(localStorage.getItem("cartData")) || [];
+    let updatedData = parseData.filter((item,index)=>index !== itemId);
+    localStorage.setItem("cartData",JSON.stringify(updatedData));
+    container.innerHTML ="";
+    getData();
+
+}
