a2efc00677e4282e88d333cc0a02aad9d854515a
Subrata <subratabera032@gmail.com>
2025-03-19 22:28:35 +0530

    uptoDay_25

--- Day_25/Assignment/Backend/routes/products.routes.js
+++
@@ -1,9 +0,0 @@
-const express=require("express");
-const { getData } = require("../controller/products.controller");
-const productRoute=express.Router();
-
-productRoute.get("/products",getData);
-
-module.exports={
-    productRoute
-}
